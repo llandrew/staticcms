4514437a108527863df3f4ed613526ea5e2c681c
Andrew Farinella <andrew@liftedlogic.com>
2016-01-20 11:47:19 -0600

    Add Container to Page Template

--- templates/page.php
+++ templates/page.php
@@ -5,24 +5,27 @@ $page_title = "Home";
 if ( isset( $_GET['page'] ) )
   $page_title = ucwords( $_GET['page'] );
 
-?>
-
-<h1><?= $page_title; ?></h1>
-
-<?php
-
-// var_dump( $_GET ); 
-
 global $db;
 
 $sql = "SELECT * FROM temp WHERE id=1";
 
 if ( ! $result = $db->query( $sql ) )
   die( "There was an error running the query [{$db->error}]." );
-
-while ( $row = $result->fetch_assoc() ) {
-
-  echo '<p>' . $row['value'] . '</p>';
-}
-
 ?>
+
+<div class="row">
+  <div class="columns">
+
+    <h1><?= $page_title; ?></h1>
+
+    <?php
+
+
+    while ( $row = $result->fetch_assoc() ) {
+
+      echo '<p>' . $row['value'] . '</p>';
+    }
+
+    ?>
+  </div>
+</div>
